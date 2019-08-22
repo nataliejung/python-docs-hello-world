a8c9f4b8013cce1c62f760268bcc87354367055b
Natalie Jung <nj_jung@hotmail.com>
2019-08-22 12:44:12 +1000

    debugging

--- application.py
+++ application.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import tika
 from tika import parser
 
+print("initiating tika")
+tika.initVM()
+print("initiating VM")
+version = tika.__version__
+print("tika version: " + version)
 
 uploadedFile = "Koc-Holding-2018-Annual-Report.pdf"
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   parsedPDF = parser.from_file("Koc-Holding-2018-Annual-Report.pdf", xmlContent=True)
-   return uploadedFile
+    print("Hello")
+    parsedPDF = parser.from_file("Koc-Holding-2018-Annual-Report.pdf", xmlContent=True)
+    return uploadedFile
