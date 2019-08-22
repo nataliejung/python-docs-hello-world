db2bb2decdbdbb78b30ea4f21de5440314228add
Natalie Jung <nj_jung@hotmail.com>
2019-08-22 12:38:05 +1000

    uploadedFile

--- application.py
+++ application.py
@@ -2,10 +2,10 @@ from flask import Flask
 from tika import parser
 
 
-
+uploadedFile = "Koc-Holding-2018-Annual-Report.pdf"
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    version = tika.__version__
-    return version
+   parsedPDF = parser.from_file("Koc-Holding-2018-Annual-Report.pdf", xmlContent=True)
+   return uploadedFile
